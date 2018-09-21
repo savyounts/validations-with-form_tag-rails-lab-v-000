bd7c53f722756ab0bc78b6a985b4bb1bc745dbca
Savannah Younts <savyounts@gmail.com>
2018-09-21 16:39:17 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,5 @@
 class AuthorsController < ApplicationController
-  before_action :set_author, only: [:edit, :update]
+  before_action :set_author, only: [:edit, :update, :show]
 
   def show
     @author = Author.find(params[:id])
